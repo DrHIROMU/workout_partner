00941449b453c2708176e22de9681ec597ddc943
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-12-21 11:15:58 +0800

    加上Firebase query

--- workout-partner-front-end/src/App.tsx
+++ workout-partner-front-end/src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import Record from "./component/record/record";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Query,
+} from "firebase/firestore/lite";
+import Excercise from "./model/exercise/excercise";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmr8q4JuzLV7Xy4YZfF8xj_5fiYvwtUQ8",
@@ -14,10 +21,23 @@ const firebaseConfig = {
   measurementId: "G-L2PX1JRHRJ",
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const fireBaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(fireBaseApp);
+const analytics = getAnalytics(fireBaseApp);
+
+async function query() {
+  const excerciesesCol: Query<any> = collection(db, "excercise");
+  const dataSnapshot = await getDocs(excerciesesCol);
+  const excercieses: Excercise[] = dataSnapshot.docs.map((doc) => doc.data());
+  console.log(excercieses);
+  return excercieses;
+}
 
 function App() {
+  useEffect(() => {
+    query();
+  });
+
   return (
     <div className="App">
       <Record></Record>
---
+++ workout-partner-front-end/src/model/exercise/excercise.tsx
@@ -0,0 +1,6 @@
+export default class Excercise {
+  bodyArea: string | undefined;
+  name: string | undefined;
+  weight: number | undefined;
+  repetition: number | undefined;
+}
